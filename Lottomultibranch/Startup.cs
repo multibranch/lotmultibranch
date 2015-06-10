cdb517e35979cdf6bbc5437d1e00c7132a2da9ca
Suphat  Phuenpha <phounpa14@gmail.com>
2015-06-10 18:03:36 +0700

    Please wait for a while

---
+++ Lottomultibranch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lottomultibranch.Startup))]
+namespace Lottomultibranch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
